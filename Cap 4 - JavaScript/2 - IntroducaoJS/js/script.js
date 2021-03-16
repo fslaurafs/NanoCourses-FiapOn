18a27496af7877aa33960c09527fcb1d856d41d7
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-03-15 21:25:01 -0300

    Introdução ao JavaScript

---
+++ Cap 4 - JavaScript/2 - IntroducaoJS/js/script.js
@@ -0,0 +1,17 @@
+/* EXEMPLO 4:
+var objBotao = document.getElementById("botao");
+
+objBotao.onclick = function() {
+    window.alert("Alô Mundo!");
+}
+*/
+
+
+/* EXEMPLO 5: */
+window.onload = function() {
+    var objBotao = document.getElementById("botao");
+
+    objBotao.onclick = function() {
+        window.alert("Alô Mundo!");
+    }
+} 
