179c342ff326ad27a7f6ea04a8f349c512571fc0
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-03-15 21:27:20 -0300

    Operadores matemáticos

---
+++ Responsive Web Development/Cap 4 - JavaScript/5 - Operadores-Matematicos/js/script.js
@@ -0,0 +1,28 @@
+console.log("Adição: 4+4: ")
+console.log(4+4);
+
+console.log("Subtração: 4-2: ")
+console.log(4-2);
+
+console.log("Multiplicação: 4x2: ")
+console.log(4*2);
+
+console.log("Divisão: 4/2: ")
+console.log(4/2);
+
+console.log("Resto da divisão de 10 por 3: ")
+console.log(10%3);
+
+
+/* Utilizando funções */
+var num = 5;
+console.log("Valor de num: " +num);
+
+num = Math.pow(num,2);
+console.log("Valor de num: " +num);
+
+num = num - Math.random();
+console.log("Valor de num: " +num);
+
+num = num * Math.PI;
+console.log("Valor de num: " +num);
