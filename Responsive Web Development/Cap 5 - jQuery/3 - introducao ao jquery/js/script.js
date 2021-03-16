bcd0e109cefa58decbca3b67d1e1e87adf58ae64
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-03-16 19:19:06 -0300

    Introdução ao jQuery

---
+++ Responsive Web Development/Cap 5 - jQuery/3 - introducao ao jquery/js/script.js
@@ -0,0 +1,11 @@
+/* UTILIZANDO O JQUERY */
+$(document).ready(function() {  /* 'ready()' garante que o HTML será renderizado primeiro */
+    console.log("Alô mundo!");
+});
+
+
+/* mesmo que:
+window.onload = function() {
+    console.log("Alô mundo!");
+}
+*/
