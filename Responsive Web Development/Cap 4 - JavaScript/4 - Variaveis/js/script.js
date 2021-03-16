c3a34c45dbea48a5e5825feac6b573c184285588
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-03-15 21:26:42 -0300

    Variáveis e seus tipos

---
+++ Responsive Web Development/Cap 4 - JavaScript/4 - Variaveis/js/script.js
@@ -0,0 +1,31 @@
+/* Exemplo de variável indefinida */
+var vIndefinida;
+console.log(typeof vIndefinida);
+
+
+/* Exemplo de booleano */
+var vBooleano = true;
+console.log(typeof vBooleano);
+
+
+/* Exemplo de alfanumérico (string) */
+var vAlfaNumerica = "Exemplo curso de JavaScript";
+console.log(typeof vAlfaNumerica);
+
+
+/* Exemplos numéricos */
+var vNumeroInteiro = -159;
+console.log(typeof vNumeroInteiro);
+
+var vNumeroFracionado = 2435.45;
+console.log(typeof vNumeroFracionado);
+
+
+/* Exemplo de array */
+var vArrayNums = [10, 20, 30];
+console.log(typeof vArrayNums);
+
+
+/* Exemplo de objeto */
+var vAluno = {name: 'Fulano de Tal', age: 18};
+console.log(typeof vAluno);
